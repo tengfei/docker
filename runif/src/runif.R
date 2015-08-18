994647d5b2a906d3ef5a836ad1ba21e749b5815d
Tengfei Yin <tengfei.yin@sbgenomics.com>
2015-08-18 18:02:00 -0400

    add report to docker

--- runif/src/runif.R
+++ runif/src/runif.R
@@ -1,17 +1,31 @@
 #!/usr/bin/Rscript
-'usage: runif.R [--n=<int> --min=<float> --max=<float> --seed=<float>]
+'usage: runif.R [--n=<int> --min=<float> --max=<float> --seed=<float> --out=<file>]
 
 options:
  --n=<int>        number of observations. If length(n) > 1, the length is taken to be the number required [default: 1].
  --min=<float>   lower limits of the distribution. Must be finite [default: 0].
  --max=<float>   upper limits of the distribution. Must be finite [default: 1].
- --seed=<float>  seed for set.seed() function [default: 1]' -> doc
+ --seed=<float>  seed for set.seed() function [default: 1]
+ --out=<file>    output file [default: ./random.txt]
+ --report=<logic> generate report or not [default: TRUE]'  -> doc
 
 library(docopt)
 opts <- docopt(doc)
 set.seed(opts$seed)
-runif(n = as.integer(opts$n), 
-      min = as.numeric(opts$min), 
-      max = as.numeric(opts$max))
+r <- runif(n = as.integer(opts$n), 
+           min = as.numeric(opts$min), 
+           max = as.numeric(opts$max))
+
+write.table(r, file = opts$out, col.names = TRUE)
+
+if(opts$report){
+    ## generate report
+    ## stop("not implemented yet")
+    ## call this
+    ## as.numeric(read.table(file = opts$out, header  = TRUE))
+    render("~/report/report.Rmd", BiocStyle::html_document(toc = TRUE),
+           output_dir = ".")
+}
+
 
 
